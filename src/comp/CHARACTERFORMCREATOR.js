13551781eb59178e214d5a9342a0c9d9d4dc472d
jorgeojara <jorgeojara@gmail.com>
2022-09-07 19:08:58 -0400

    Something

--- src/comp/CHARACTERFORMCREATOR.js
+++ src/comp/CHARACTERFORMCREATOR.js
@@ -833,7 +833,7 @@ name="group"
 <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
 </div>
   <button className="btn btn-secondary b"
-  onClick={()=>console.log(autoBuild.giveMeABirthPlace(roll(100),roll(100)))}
+  onClick={()=>autoBuild.giveMeABirthPlace(roll(100),roll(100))}
   >
         <i className="text-danger fas fa-trash o">Create</i>
 </button>
