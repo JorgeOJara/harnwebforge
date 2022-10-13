0c25c5b70f636cd40138cfee69b70a53e2624684
JorgeOJara <31555704+JorgeOJara@users.noreply.github.com>
2022-10-13 10:35:02 -0400

    hate

--- server.js
+++ server.js
@@ -1,52 +1,33 @@
-<<<<<<< HEAD
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
 const https = require('https');
 const http = require('http');
+const cors = require("cors")
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
+
+app.use(cors());
+// Declare static folder to be served. It contains the JavaScript code, images, CSS, etc.
 app.use(express.static('build'));
-app.use(cors());
-=======
-const https = require('https');
-const http = require('http');
-
-const fs = require('fs');
-
->>>>>>> 3d1afbd646e27f17a36a7bf5612183bc1b3fda94
-
-const app = express();
-app.use(cors());
 
 // create new express app and save it as "app"
 app.get('/', function (req, res) {
-  res.sendFile(path.join(dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.get('/login', function (req, res) {
-<<<<<<< HEAD
-     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-=======
-  res.sendFile(path.join(dirname, 'build', 'index.html'));
->>>>>>> 3d1afbd646e27f17a36a7bf5612183bc1b3fda94
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.get('*', function(req, res){
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Listen both http & https ports
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-<<<<<<< HEAD
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
+serverhttps.createServer({    
+    key: fs.readFileSync('./key.pem'),    
+    cert: fs.readFileSync('./cert.pem'),   
     passphrase: 'lokita1212'
-=======
-  key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
->>>>>>> 3d1afbd646e27f17a36a7bf5612183bc1b3fda94
-}, app);
+  }, app)
 
 httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
@@ -54,8 +35,4 @@ httpServer.listen(80, () => {
 
 httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443');
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 3d1afbd646e27f17a36a7bf5612183bc1b3fda94
