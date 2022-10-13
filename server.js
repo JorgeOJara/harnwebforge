7497bb725b8fdc746575d8f23e2c091edca83d8f
jorge O jara <jorgeojara@gmail.com>
2022-10-13 12:55:32 +0000

    cloud

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+let done  = 0;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
