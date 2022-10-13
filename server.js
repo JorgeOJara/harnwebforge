5d847c7b160f14ddf0cbbc969ad0a22c78eb6b8b
JorgeOJara <31555704+JorgeOJara@users.noreply.github.com>
2022-10-13 10:42:44 -0400

    done

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use(cors());
 // Declare static folder to be served. It contains the JavaScript code, images, CSS, etc.
 app.use(express.static('build'));
 
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/harnforge.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/harnforge.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/harnforge.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
 // create new express app and save it as "app"
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -23,16 +35,12 @@ res.sendFile(path.join(__dirname, 'build', 'index.html'));
 
 // Listen both http & https ports
 const httpServer = http.createServer(app);
-httpServer.createServer({    
-    key: fs.readFileSync('./key.pem'),    
-    cert: fs.readFileSync('./cert.pem'),   
-    passphrase: 'lokita1212'
-  }, app)
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
 });
 
-httpServer.listen(443, () => {
+httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443');
 });
