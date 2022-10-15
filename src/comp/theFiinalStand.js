3b89d70bba899ac0ea257efe9736c45f64e9aee4
jorgeojara <jorgeojara@gmail.com>
2022-10-15 19:42:19 -0400

    Something

--- src/comp/theFiinalStand.js
+++ src/comp/theFiinalStand.js
@@ -16,8 +16,7 @@ checkforAll()
   if(this.obj.Race == ""){this.obj.Race = this.helper.chooseRaceForMe(this.helper.roll(100))}
   if(this.obj.Sex == ""){this.obj.Sex = this.helper.chooseSexForMe(this.helper.roll(100),this.obj.Race)}
   if(this.obj.Sunsigns == ""){this.obj.Sunsigns = this.helper.chooseMyBirthdayForMe("undefined","undefined")}
-  if(this.obj.Culture == "")
-  {let done = this.helper.giveMeAsocialClass(this.helper.roll(100),this.obj.Culture,this.obj.birthPlace);this.obj.Culture = done[0]}
+  if(this.obj.Culture == ""){this.obj.Culture = this.helper.giveMebackMyculture(this.helper.roll(100));}
   if(this.obj.giveBackbirthplace == ""){this.obj.birthPlace = this.helper.birthPlace(this.obj.Culture,this.helper.roll(100))}
   if(this.obj.SocialClass == "")
   {let done  = this.helper.giveMeAsocialClass(this.helper.roll(100),this.obj.Culture,this.obj.birthPlace) ; this.obj.SocialClass = done[1];}
