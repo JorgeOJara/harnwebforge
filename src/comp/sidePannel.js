00829609203177fa1b5cbb7f27b4f54021874a9b
jorgeojara <jorgeojara@gmail.com>
2022-10-18 23:13:45 -0400

    Something

--- src/comp/sidePannel.js
+++ src/comp/sidePannel.js
@@ -52,7 +52,7 @@ Your Characters
 {
 completed.map(item => {
     return <button className="btn btn-secondary ch"
-             onClick={ () => go( item.id ) }
+             onClick={ () => go( item.Name ) }
             >
           <i className="none">{item.Name}</i>
       </button>
