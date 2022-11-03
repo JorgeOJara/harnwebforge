01bbdde91ea2eee29c7e7d80ec55c7747872dd47
jorgeojara <jorgeojara@gmail.com>
2022-11-03 17:20:13 -0400

    Something

--- src/comp/displayMe.js
+++ src/comp/displayMe.js
@@ -15,7 +15,7 @@ let pen = { name : props.content , member : localStorage.getItem("username")}
 const findC = ()=>{
 axios.post('https://harnforge.com/idsFinder', pen)
 .then(function(response) {
-  ddoijhdbeiu(response.data);
+  ddoijhdbeiu(JSON.parse(response.data));
   console.log(response.data.items);
  })
 }
