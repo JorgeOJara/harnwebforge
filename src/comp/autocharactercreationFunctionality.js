8f0dc63958afc8880df2ee0db3ed885179d554c2
jorgeojara <jorgeojara@gmail.com>
2022-11-26 22:45:34 -0500

    Something

--- src/comp/autocharactercreationFunctionality.js
+++ src/comp/autocharactercreationFunctionality.js
@@ -443,7 +443,7 @@ let Culture;
 // pick a  ramdom ocupations from avaliable ocupations....
      let finalOcupation = this.roll(ocupation.length)
      let ajustOcupation = finalOcupation - 1;
-
+     console.log(ocupation)
       return ocupation[ajustOcupation];
 
       /// I need to know how to pick a ocupations from the possible list of opcupations already given....
